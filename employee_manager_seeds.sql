332d61680dc5ffbdc50dcb99753cd66dbc79c917
Peter Phenow <peter.phenow@gmail.com>
2020-12-20 21:13:57 -0600

    made changes so data would insert without errors after creating foreign keys

--- employee_manager_seeds.sql
+++ employee_manager_seeds.sql
@@ -1,15 +1,13 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, NULL),
-	   ("Arjun", "Valery", 2, 1),
-       ("Devi", "Waldeburg", 3, NULL),
-       ("Igor", "Linda", 4, 3),
-       ("Lyle", "Ibrahim", 5, NULL),
-       ("Monica", "Pilar", 6, 5),
-       ("Isabella", "Benson", 7, NULL),
-       ("Lakshmi", "Agrippa", 8, 7);
-       
-SELECT * FROM employee;
-       
+USE employee_manager_db;
+
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+
+SELECT * FROM department;
+
 INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 90000, 1),
        ("Salesperson", 70000, 1),
@@ -20,12 +18,20 @@ VALUES ("Sales Lead", 90000, 1),
        ("Legal Team Lead", 190000, 4),
        ("Lawyer", 150000, 4);
        
-SELECT * FROM role;       
+SELECT * FROM role;    
 
-INSERT INTO department (name)
-VALUES ("Sales"),
-       ("Engineering"),
-       ("Finance"),
-       ("Legal");
+-- first add employees without managers
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("John", "Doe", 1),
+	   ("Devi", "Waldeburg", 3),
+       ("Lyle", "Ibrahim", 5),
+       ("Isabella", "Benson", 7);
 
-SELECT * FROM department;
+-- then add employees with managers
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Arjun", "Valery", 2, 1),
+       ("Igor", "Linda", 4, 2),
+       ("Monica", "Pilar", 6, 3),
+       ("Lakshmi", "Agrippa", 8, 4);
+       
+SELECT * FROM employee;
